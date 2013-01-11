1a5e0b3dab17368d70564a761c123aba0c1f5254
JDrit <jd@csh.rit.edu>
2013-01-11 18:24:04 -0500

    Started work on the HTTPS connection with SSL that will talk to webnews. Added httpsclient and data structures

--- src/edu/rit/csh/androidwebnews/HttpsConnector.java
+++ src/edu/rit/csh/androidwebnews/HttpsConnector.java
@@ -1,36 +1,58 @@
 package edu.rit.csh.androidwebnews;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-public class HttpsConnector {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class HttpsConnector extends AsyncTask<String, Integer, String> {
 	String apiKey;
+	String mainUrl = "https://webnews.csh.rit.edu";
+	WebnewsHttpClient httpclient;
 	
-	public HttpsConnector(String apiKey) {
+	public HttpsConnector(String apiKey, Activity activity) {
 		this.apiKey = apiKey;
+		httpclient = new WebnewsHttpClient(activity.getApplicationContext());
 	}
 	
-	public ArrayList<String> getNewsGroups() {
+	public ArrayList<Newsgroup> getNewsGroups() {
+        return null;
 		
 	}
 	
 	public ArrayList<Thread> getUnread() {
-		
+		return null;
 	}
 	
 	public ArrayList<Thread> getNewsgroupThreads(String name, int amount) {
-		
-	}
-	
-	public ArrayList<Thread> getUnread() {
-		
+		return null;
 	}
 	
 	public String getPostBody(String newsgroup, int id) {
-		
+		return null;
 	}
 	
 	public ArrayList<Thread> getNewsgroupThreadsDate(String date, int amount) {
-		
+		return null;
 	}
 	
 	public void markRead() {
@@ -42,6 +64,50 @@ public class HttpsConnector {
 	}
 	
 	private ArrayList<Thread> makeThreads(String jsonObj) {
-		
+		return null;
 	}
+	
+	private String addApiToUrl(String url) {
+		if (!url.endsWith("?")) {
+			url += "?";
+		}
+		List<NameValuePair> params = new LinkedList<NameValuePair>();
+		params.add(new BasicNameValuePair("api_key", apiKey));
+		params.add(new BasicNameValuePair("api_agent", "Android_Webnews"));
+		String paramString = URLEncodedUtils.format(params,  "utf-8");
+		url += paramString;
+		return url;
+	}
+
+	@Override
+	protected String doInBackground(String... params) {
+		try {
+        	HttpGet request = new HttpGet(mainUrl);
+			Log.d("AndroidWebnews", mainUrl);
+			HttpResponse response = httpclient.execute(request);
+			HttpEntity responseEntity = response.getEntity();
+			BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			
+			StringBuffer sb = new StringBuffer("");
+            String line = "";
+            String NL = System.getProperty("line.separator");
+            
+            while ((line = in.readLine()) != null) {
+            	Log.d("AndroidWebnews", line);
+                sb.append(line + NL);
+            }
+            in.close();
+            String page = sb.toString();
+            Log.d("test", page);
+    		return page;
+            
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
+
+	
 }
