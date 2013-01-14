3d2174f05a96f510dd7fc0d4ce3202fd14711a54
JDrit <jd@csh.rit.edu>
2013-01-14 13:26:22 -0500

    Fixed an issue in the webnews list adapter getViewGroup() not being able to use the view button. Added layout files.

--- src/edu/rit/csh/androidwebnews/newsgroupActivity.java
+++ src/edu/rit/csh/androidwebnews/newsgroupActivity.java
@@ -42,12 +42,10 @@ public class newsgroupActivity extends Activity{
 	}
 	
 	public void viewPost(View view) {
-		Log.d("name", view.getContentDescription().toString().substring(0, view.getContentDescription().toString().indexOf('/')).toString());
-		Log.d("id", view.getContentDescription().toString().substring(view.getContentDescription().toString().indexOf('/') + 1).toString());
 		
 		Intent intent = new Intent(this, PostActivity.class);
-		intent.putExtra("SELECTED_NEWSGROUP", view.getContentDescription().toString().substring(0, view.getContentDescription().toString().indexOf('/')).toString());
-		intent.putExtra("SELECTED_ID", Integer.valueOf(view.getContentDescription().toString().substring(view.getContentDescription().toString().indexOf('/') + 1).toString()));
+		intent.putExtra("SELECTED_NEWSGROUP", ((Thread) view.getTag()).newsgroup);
+		intent.putExtra("SELECTED_ID", ((Thread) view.getTag()).number);
 
 		Log.d("des", "intent made");
 		startActivity(intent);
