5aa43e97a34e41d5c8ddda84fad4995f81a7096a
JDrit <jd@csh.rit.edu>
2013-01-15 04:23:41 -0500

    Added a settings page to the application

---
+++ src/edu/rit/csh/androidwebnews/SettingsFragment.java
@@ -0,0 +1,18 @@
+package edu.rit.csh.androidwebnews;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SettingsFragment extends Fragment {
+	
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View convertView = inflater.inflate(R.layout.threadlayout, null);
+		
+		
+		return convertView;
+	}
+
+}
