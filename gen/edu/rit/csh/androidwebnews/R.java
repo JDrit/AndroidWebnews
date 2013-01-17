db9d1f8a22fcf97062dfc29d01212d9ada6878b9
JDrit <jd@csh.rit.edu>
2013-01-17 15:06:37 -0500

    Changed the post viewing to use async task without stalling.

--- gen/edu/rit/csh/androidwebnews/R.java
+++ gen/edu/rit/csh/androidwebnews/R.java
@@ -25,25 +25,26 @@ public final class R {
     }
     public static final class id {
         public static final int LicenseButton=0x7f0a0003;
-        public static final int Viewbutton=0x7f0a0010;
+        public static final int Viewbutton=0x7f0a0011;
         public static final int aboutButton=0x7f0a0002;
         public static final int aboutTextView=0x7f0a0001;
-        public static final int imageView1=0x7f0a000e;
+        public static final int imageView1=0x7f0a000f;
         public static final int list=0x7f0a0004;
-        public static final int mainListView=0x7f0a0009;
-        public static final int menu_about=0x7f0a0013;
-        public static final int menu_refresh=0x7f0a0011;
-        public static final int menu_settings=0x7f0a0012;
+        public static final int mainListView=0x7f0a000a;
+        public static final int menu_about=0x7f0a0014;
+        public static final int menu_refresh=0x7f0a0012;
+        public static final int menu_settings=0x7f0a0013;
         public static final int newsgroup_list=0x7f0a0000;
-        public static final int pager=0x7f0a0005;
-        public static final int recent_fragment=0x7f0a0007;
-        public static final int recent_text_view=0x7f0a0006;
-        public static final int replyButton=0x7f0a000a;
-        public static final int rowTextView=0x7f0a000c;
-        public static final int threadrlayout=0x7f0a000d;
-        public static final int threadsfragment=0x7f0a0008;
-        public static final int threadtextview=0x7f0a000f;
-        public static final int unreadButton=0x7f0a000b;
+        public static final int pager=0x7f0a0006;
+        public static final int post_fragment=0x7f0a0005;
+        public static final int recent_fragment=0x7f0a0008;
+        public static final int recent_text_view=0x7f0a0007;
+        public static final int replyButton=0x7f0a000b;
+        public static final int rowTextView=0x7f0a000d;
+        public static final int threadrlayout=0x7f0a000e;
+        public static final int threadsfragment=0x7f0a0009;
+        public static final int threadtextview=0x7f0a0010;
+        public static final int unreadButton=0x7f0a000c;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
