f7d8dd6497bb9144c8b5f927b59ce9a2a2c714e2
JDrit <jd@csh.rit.edu>
2013-01-22 18:57:31 -0500

    Fixed issue with the time between check stores correctly and finished making the ability to compose messages.

--- src/edu/rit/csh/androidwebnews/HttpsConnector.java
+++ src/edu/rit/csh/androidwebnews/HttpsConnector.java
@@ -502,8 +502,9 @@ public class HttpsConnector {
 			BasicNameValuePair bodyVP = new BasicNameValuePair("body", body);
 			BasicNameValuePair newsgroupParentVP = new BasicNameValuePair("reply_newsgroup", newsgroupParent);
 			BasicNameValuePair idParentVP = new BasicNameValuePair("reply_newsgroup", Integer.valueOf(parentId).toString());
+			BasicNameValuePair stickyVP = new BasicNameValuePair("sticky_until" , "1 second from now");
 			
-			new HttpsPostAsyncTask(httpclient).execute(urlVP, newsgroupVP, subjectVP, bodyVP, newsgroupParentVP, idParentVP);
+			new HttpsPostAsyncTask(httpclient).execute(urlVP, newsgroupVP, subjectVP, bodyVP, newsgroupParentVP, idParentVP, stickyVP);
 		} else {
 			dialog.show();
 		}
